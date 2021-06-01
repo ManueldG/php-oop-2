58046152957fba5f4c324855557841387e392adb
Manuel della Gala <manuel.dellagala@gmail.com>
2021-06-01 15:49:39 +0200

    ok

--- php-oop-2/Shop.php
+++ php-oop-2/Shop.php
@@ -7,9 +7,17 @@
 class Shop{
     
     protected  $articolo;
+    protected  $tipo;
+    protected  $prezzo;
+    protected  $descrizione;
     
-    public function __construct()
-    {
+    
+
+    public function __construct($articolo){
+        $this->articolo = $articolo;
+        $this->tipo = '';
+        $this->prezzo = '';
+        $this->descrizione= '';
         
     }
 
@@ -28,7 +36,56 @@ class Shop{
     {
         $this->articolo = $articolo;
     }
-
+    
+    /**
+     * @return string
+     */
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getPrezzo()
+    {
+        return $this->prezzo;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getDescrizione()
+    {
+        return $this->descrizione;
+    }
+    
+    /**
+     * @param string $tipo
+     */
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+    
+    /**
+     * @param string $prezzo
+     */
+    public function setPrezzo($prezzo)
+    {
+        $this->prezzo = $prezzo;
+    }
+    
+    /**
+     * @param string $descrizione
+     */
+    public function setDescrizione($descrizione)
+    {
+        $this->descrizione = $descrizione;
+    }
+    
+    
     
 }
 
